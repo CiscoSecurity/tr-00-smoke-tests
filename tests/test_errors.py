2287da7bd4bd3e9964026365ed2e62b09b021c9d
Pavlo Havrylov <TravisNotifications@softserveinc.com>
2020-10-12 19:53:18 +0300

    Test

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -25,7 +25,6 @@ def test_smoke_integrations_errors(observables, module_headers):
 
     errors = response_from_all_modules.get('errors', [])
     errors_info = dict()
-
     sleep(30)
 
     if errors:
